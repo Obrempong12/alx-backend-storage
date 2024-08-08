233c8de9b16fbfa996f39a994b6ffd9bef034be0
Obrempong12 <tuntumbrane@gmail.com>
2024-08-08 14:26:09 +0000

    MySQL Advanced

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,21 @@
+-- Here, the script creates a func SafeDiv that divides (and returns) the first
+-- by the second number or returns 0 if the second number is equal to 0.
+DROP FUNCTION IF EXISTS SafeDiv;
+
+DELIMITER $$
+
+CREATE FUNCTION SafeDiv(
+        a INT,
+        b INT
+)
+RETURNS FLOAT
+DETERMINISTIC
+BEGIN
+        IF b = 0 THEN
+	    RETURN 0;
+        END IF;
+
+        RETURN a / b;
+END $$
+
+DELIMITER ;
